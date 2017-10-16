da15ad046a3527c10c0e2f489b3eaf5fa8a380b4
Rich Nistuk <rnistuk@gmail.com>
2017-10-16 09:35:33 -0700

    Prepping for websocket data.

--- bluzelled/inc/WebSocket.h
+++ bluzelled/inc/WebSocket.h
@@ -7,48 +7,72 @@
 #include <boost/thread.hpp>
 #include <boost/algorithm/string/join.hpp>
 #include <sstream>
+#include <fstream>
 
 // {cmds:[{cmd: 'addNode', data:{address: '0xffff', status: 'alive'}}]}
 
 class WebSocket {
     Nodes*          _nodes;
     boost::thread*  _thread;
-
+    std::string  node_list();
 public:
     WebSocket(Nodes* nodes) : _nodes(nodes) {
         auto server = [this]()
         {
             uWS::Hub h;
-            h.onMessage([](uWS::WebSocket<uWS::SERVER>* ws, char *message, size_t length, uWS::OpCode opCode) {
-                ws->send(message, length, opCode);
+            h.onMessage([this](uWS::WebSocket<uWS::SERVER>* ws, char *message, size_t length, uWS::OpCode opCode) {
+                ws->send(this->node_list().c_str(), this->node_list().length(), opCode);
                 });
 
             h.onHttpRequest([this](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t length, size_t remainingBytes) {
                 std::stringstream ss;
-                ss << "[";
-               for(auto n : *this->_nodes)
-                   {
-                   ss << "\"" << n->get_thread_id() << "\",";
-                   }
-                ss.seekp(-1, ss.cur);
-                ss << "]\n";
+                std::ifstream file;
+                file.open("../index.html");
+                if(file.is_open())
+                    {
+                    std::string line;
+                    while(std::getline(file,line))
+                        {
+                        ss << line;
+                        }
+                    }
+                else
+                    {
+                    std::cerr << "Unable to open index.html.\n";
+                    }
+
                 res->end(ss.str().c_str(), ss.str().length());
                 });
 
+//            h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
+//                // send this client all stored messages in one batch send
+//                uWS::WebSocket<uWS::SERVER>::PreparedMessage *preparedMessageBatch = uWS::WebSocket<uWS::SERVER>::prepareMessageBatch(storedMessages, excludedMessages, uWS::TEXT, false);
+//                ws.sendPrepared(preparedMessageBatch);
+//                ws.finalizeMessage(preparedMessageBatch);
+//
+//                // broadcast number of clients connected to everyone
+////                std::string tmp = "S " + std::to_string(++connections) + " " +  std::to_string(getKb());
+////                h.getDefaultGroup<uWS::SERVER>().broadcast(tmp.data(), tmp.length(), uWS::TEXT);
+//                });
 
-            h.listen(3000);
+
+
+
+
+            if(h.listen(3000))
+                {
+                std::cout << "Listening to port 3000" << std::endl;
+                }
+            else
+                {
+                std::cerr << "Failed to listen to port" << std::endl;
+                return -1;
+                }
+
             h.run();
         };
         _thread = new boost::thread(server);
     };
-    ~WebSocket()
-    {
-//        _thread->join();
-//        delete _thread;
-//        _nodes = nullptr;
-    };
-
-
 };
 
 #endif //KEPLER_WEBSOCKET_H
--- bluzelled/src/WebSocket.cpp
+++ bluzelled/src/WebSocket.cpp
@@ -1,1 +1,16 @@
 #include "WebSocket.h"
+
+
+
+std::string WebSocket::node_list()
+{
+    std::stringstream ss;
+    ss << "[";
+    for(auto n : *this->_nodes)
+        {
+        ss << "\"" << n->get_thread_id() << "\",";
+        }
+    ss.seekp(-1, ss.cur);
+    ss << "]\n";
+    return ss.str();
+}
