09010cf784945ece864a9ce120dfcda47fc24e38
Rich Nistuk <rnistuk@gmail.com>
2017-10-16 11:59:35 -0700

    Added ping and nodes

--- bluzelled/inc/WebSocket.h
+++ bluzelled/inc/WebSocket.h
@@ -15,64 +15,51 @@ class WebSocket {
     Nodes*          _nodes;
     boost::thread*  _thread;
     std::string  node_list();
+
+    void on_message(uWS::WebSocket<uWS::SERVER>* ws, char *message, size_t length, uWS::OpCode opCode)
+    {
+        message[length] = 0;
+        std::string response("");
+        std::string command(message);
+        // replace with stategty pattern
+        if( 0 == command.compare("ping") )
+            {
+            response = "pong";
+            }
+        else if( 0 == command.compare("getAllNodes") )
+            {
+            response = this->node_list();
+            }
+        ws->send( response.c_str(), response.length(), opCode);
+    }
+
+    void on_http_request(
+            uWS::HttpResponse *res,
+            uWS::HttpRequest req,
+            char *data,
+            size_t length,
+            size_t remainingBytes)
+    {
+        std::stringstream ss;
+        std::ifstream file;
+        file.open("../web/index.html");
+        if(file.is_open())
+            {
+            std::string line;
+            while(std::getline(file,line))
+                {
+                ss << line;
+                }
+            }
+        else
+            {
+            std::cerr << "Unable to open index.html.\n";
+            }
+        res->end(ss.str().c_str(), ss.str().length());
+    }
+
 public:
-    WebSocket(Nodes* nodes) : _nodes(nodes) {
-        auto server = [this]()
-        {
-            uWS::Hub h;
-            h.onMessage([this](uWS::WebSocket<uWS::SERVER>* ws, char *message, size_t length, uWS::OpCode opCode) {
-                ws->send(this->node_list().c_str(), this->node_list().length(), opCode);
-                });
-
-            h.onHttpRequest([this](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t length, size_t remainingBytes) {
-                std::stringstream ss;
-                std::ifstream file;
-                file.open("../index.html");
-                if(file.is_open())
-                    {
-                    std::string line;
-                    while(std::getline(file,line))
-                        {
-                        ss << line;
-                        }
-                    }
-                else
-                    {
-                    std::cerr << "Unable to open index.html.\n";
-                    }
-
-                res->end(ss.str().c_str(), ss.str().length());
-                });
-
-//            h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
-//                // send this client all stored messages in one batch send
-//                uWS::WebSocket<uWS::SERVER>::PreparedMessage *preparedMessageBatch = uWS::WebSocket<uWS::SERVER>::prepareMessageBatch(storedMessages, excludedMessages, uWS::TEXT, false);
-//                ws.sendPrepared(preparedMessageBatch);
-//                ws.finalizeMessage(preparedMessageBatch);
-//
-//                // broadcast number of clients connected to everyone
-////                std::string tmp = "S " + std::to_string(++connections) + " " +  std::to_string(getKb());
-////                h.getDefaultGroup<uWS::SERVER>().broadcast(tmp.data(), tmp.length(), uWS::TEXT);
-//                });
-
-
-
-
-
-            if(h.listen(3000))
-                {
-                std::cout << "Listening to port 3000" << std::endl;
-                }
-            else
-                {
-                std::cerr << "Failed to listen to port" << std::endl;
-                return -1;
-                }
-
-            h.run();
-        };
-        _thread = new boost::thread(server);
-    };
+    WebSocket(Nodes* nodes);
 };
 
 #endif //KEPLER_WEBSOCKET_H
--- bluzelled/src/WebSocket.cpp
+++ bluzelled/src/WebSocket.cpp
@@ -1,6 +1,50 @@
 #include "WebSocket.h"
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+WebSocket::WebSocket(Nodes *nodes) : _nodes(nodes)
+{
+    auto server = [this]()
+        {
+        uWS::Hub h;
+        h.onMessage(
+                [this](uWS::WebSocket<uWS::SERVER>* ws, char *message, size_t length, uWS::OpCode opCode)
+                    {
+                    this->on_message(ws, message, length, opCode);
+                    });
 
+        h.onHttpRequest(
+                [this](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t length, size_t remainingBytes)
+                    {
+                    this->on_http_request(res, req, data, length, remainingBytes);
+                    });
 
+            h.onConnection([this](uWS::WebSocket<uWS::SERVER>* ws, uWS::HttpRequest req) {
+                std::cout << "ws:[" << ws->remainingBytes << "]\n";
+//                // send this client all stored messages in one batch send
+//                uWS::WebSocket<uWS::SERVER>::PreparedMessage *preparedMessageBatch = uWS::WebSocket<uWS::SERVER>::prepareMessageBatch(storedMessages, excludedMessages, uWS::TEXT, false);
+//                ws.sendPrepared(preparedMessageBatch);
+//                ws.finalizeMessage(preparedMessageBatch);
+//
+//                // broadcast number of clients connected to everyone
+//                std::string tmp = "S " + std::to_string(++connections) + " " +  std::to_string(getKb());
+//                h.getDefaultGroup<uWS::SERVER>().broadcast(tmp.data(), tmp.length(), uWS::TEXT);
+                });
+
+        if(h.listen(3000))
+            {
+            std::cout << "Listening to port 3000" << std::endl;
+            }
+        else
+            {
+            std::cerr << "Failed to listen to port" << std::endl;
+            return -1;
+            }
+
+        h.run();
+        };
+
+    _thread = new boost::thread(server);
+}
 
 std::string WebSocket::node_list()
 {
