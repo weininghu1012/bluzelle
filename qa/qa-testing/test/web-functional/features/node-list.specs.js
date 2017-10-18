3aeaff23b1d108480937a95971ace8f8aded1992
Ffarag <fatma@bluzelle.com>
2017-10-19 01:07:06 +0300

    Creating a new test for the Node List tab.

---
+++ qa/qa-testing/test/web-functional/features/node-list.specs.js
@@ -0,0 +1,15 @@
+describe('@watch Node List tab', () => {
+
+    describe('Table Headers', () => {
+        const header = require('../getBaseElement')('div.react-grid-HeaderRow');
+        beforeEach(() => {
+            browser.waitForExist('=Node List', 2000);
+            browser.click('=Node List');
+        });
+       it('should contain table headers', () => {
+          ['Address', 'Status', 'Messages'].forEach(text => {
+             header.waitForExist (`div.widget-HeaderCell__value*=${text}`);
+          });
+       });
+    });
+});
