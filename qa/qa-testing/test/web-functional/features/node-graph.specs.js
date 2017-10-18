3076185a15c98b89f3376b8f1403b5302f1fa0dc
scott <scott@bulldoginfo.com>
2017-10-17 20:12:30 -0700

    removed @watch

--- qa/qa-testing/test/web-functional/features/node-graph.specs.js
+++ qa/qa-testing/test/web-functional/features/node-graph.specs.js
@@ -2,7 +2,7 @@ describe('Node graph tab', () => {
     require('../base').getBase('div');
 
     describe('individual nodes', () => {
-        it('@watch should display specs when mouseover', () => {
+        it('should display specs when mouseover', () => {
             browser.waitForExist('=Node Graph', 5000);
             browser.click('=Node Graph');
             browser.waitForExist('circle[fill="green"]');
