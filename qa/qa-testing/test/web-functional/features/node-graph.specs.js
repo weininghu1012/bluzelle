6519f7ebccc063aca3b6db70ad4bba2d091262ce
scott <scott@bulldoginfo.com>
2017-10-17 20:18:24 -0700

    moved setup to beforeEach

--- qa/qa-testing/test/web-functional/features/node-graph.specs.js
+++ qa/qa-testing/test/web-functional/features/node-graph.specs.js
@@ -1,10 +1,13 @@
 describe('Node graph tab', () => {
     require('../base').getBase('div');
 
+    beforeEach(() => {
+        browser.waitForExist('=Node Graph');
+        browser.click('=Node Graph');
+    });
+
     describe('individual nodes', () => {
         it('should display specs when mouseover', () => {
-            browser.waitForExist('=Node Graph');
-            browser.click('=Node Graph');
             browser.waitForExist('circle[fill="green"]');
             browser.moveToObject('circle[fill="green"]');
             browser.waitForExist('td=alive');
