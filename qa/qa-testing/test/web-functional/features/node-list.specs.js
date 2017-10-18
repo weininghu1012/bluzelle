920a0807ab60eb032bacb819a738ad654e358f75
scott <scott@bulldoginfo.com>
2017-10-18 15:11:12 -0700

    fixed my mistake

--- qa/qa-testing/test/web-functional/features/node-list.specs.js
+++ qa/qa-testing/test/web-functional/features/node-list.specs.js
@@ -1,14 +1,16 @@
 describe('@watch Node List tab', () => {
 
     describe('Table Headers', () => {
-        const header = require('../getBaseElement')('div.react-grid-HeaderRow');
+        require('../getBaseElement')('div.react-grid-HeaderRow');
+
         beforeEach(() => {
             browser.waitForExist('=Node List', 2000);
             browser.click('=Node List');
         });
        it('should contain table headers', () => {
+           const header = browser.element('div.react-grid-HeaderRow');
           ['Address', 'Status', 'Messages'].forEach(text => {
-             header.waitForExist (`div.widget-HeaderCell__value*=${text}`);
+            header.waitForExist(`div.widget-HeaderCell__value*=${text}`);
           });
        });
     });
