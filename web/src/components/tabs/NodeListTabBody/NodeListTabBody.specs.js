db6616ce2cb1b2d737e7999ff44cfb71785ecfbb
scott <scott@bulldoginfo.com>
2017-10-19 06:29:02 -0700

    Added new test

---
+++ web/src/components/tabs/NodeListTabBody/NodeListTabBody.specs.js
@@ -0,0 +1,8 @@
+import NodeList from './NodeListTabBody'
+
+describe('components/NodeListTabBody', () => {
+    it('should render without errors if there are no nodes', () => {
+        const wrapper = shallow(<NodeList/>)
+        expect(wrapper).to.have.html().match(/address/);
+    });
+});
