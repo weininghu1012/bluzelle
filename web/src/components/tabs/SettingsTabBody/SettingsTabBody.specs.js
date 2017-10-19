c154ed7b2b29406dadd4fc5984eb38d8efb52043
scott <scott@bulldoginfo.com>
2017-10-19 08:24:42 -0700

    added tests

---
+++ web/src/components/tabs/SettingsTabBody/SettingsTabBody.specs.js
@@ -0,0 +1,7 @@
+import SettingsTabBody from './SettingsTabBody'
+
+describe('tabs/SettingsTabBody', () => {
+    it('should mount without errors', () => {
+        const wrapper = mount(<SettingsTabBody/>);
+    });
+});
