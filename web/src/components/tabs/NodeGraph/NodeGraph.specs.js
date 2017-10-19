777679c35fd7fb3109d3b631ffe99a215d015ef4
scott <scott@bulldoginfo.com>
2017-10-19 11:57:59 -0700

    fix test

--- web/src/components/tabs/NodeGraph/NodeGraph.specs.js
+++ web/src/components/tabs/NodeGraph/NodeGraph.specs.js
@@ -1,8 +1,8 @@
 import NodeGraph from './NodeGraph'
 
-describe('components/NodeGraph/NodeGraph', () => {
+describe('components/NodeGraph', () => {
     it('should render without errors if there are no nodes', () => {
-        const wrapper = mount(<NodeGraph/>)
+        const wrapper = shallow(<NodeGraph/>)
         expect(wrapper).to.have.html().match(/0 Nodes/);
         wrapper.unmount();
     });
