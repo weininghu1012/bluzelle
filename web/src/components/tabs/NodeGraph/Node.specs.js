ae9520ae64369504cdd59ed18140370e9fe41e87
scott <scott@bulldoginfo.com>
2017-10-19 07:26:20 -0700

    New interface tests

--- web/src/components/tabs/NodeGraph/Node.specs.js
+++ web/src/components/tabs/NodeGraph/Node.specs.js
@@ -4,7 +4,19 @@ console.log(Node);
 
 describe('tabs/NodeGraph/Node', () => {
     it('should mount without errors', () => {
-        const wrapper = mount(<Node node={{address: 1}} onMouseOver={() => {}} selected={false}/>);
-        expect(wrapper).to.have.html().match(/\<circle/);
-    })
+        const wrapper = mount(<Node node={{address: 1, xAngle: 0, yAngle: 0}} onMouseOver={() => {}} selected={false}/>);
+        expect(wrapper).to.have.html('<circle cx="100" cy="100" r="3"></circle>');
+    });
+
+    it('should not have a second circle when not selected', () => {
+        it('should include another circle when selected', () => {
+            const wrapper = mount(<Node node={{address: 1, xangle: 0, yAngle: 0}} onMouseOver={() => {}} selected={false}/>)
+            expect(wrapper).to.have.exactly(1).descendants('circle');
+        });
+    });
+
+    it('should include another circle when selected', () => {
+        const wrapper = mount(<Node node={{address: 1, xangle: 0, yAngle: 0}} onMouseOver={() => {}} selected={true}/>)
+        expect(wrapper).to.have.exactly(2).descendants('circle');
+    });
 });
