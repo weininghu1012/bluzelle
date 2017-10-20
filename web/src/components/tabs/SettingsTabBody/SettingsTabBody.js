69e55a0e0777f27b6cb9b842e1545a2f23c7ac65
scott <scott@bulldoginfo.com>
2017-10-19 17:10:18 -0700

    Don't allow editing of input while setting new value

--- web/src/components/tabs/SettingsTabBody/SettingsTabBody.js
+++ web/src/components/tabs/SettingsTabBody/SettingsTabBody.js
@@ -1,5 +1,6 @@
 import {settings, setMaxNodes} from 'services/SettingsService'
 import Button from 'react-bootstrap/lib/Button'
+import invoke from 'lodash/invoke'
 
 @observer
 export default class SettingsTabBody extends Component {
@@ -11,22 +12,61 @@ export default class SettingsTabBody extends Component {
     render() {
         return (
             <div style={{padding: 20}}>
-            <Table>
-                <Row id="max-nodes-setting" label="Max Nodes" setFn={this.doSetMaxNodes.bind(this)}>
-                    <input key={settings.maxNodes} ref={r => this.maxNodesInput = r} type="number" defaultValue={settings.maxNodes} />
-                </Row>
-            </Table>
+                <Table>
+                    <Row id="max-nodes-setting" label="Max Nodes" setFn={this.doSetMaxNodes.bind(this)} value={settings.maxNodes}>
+                        <input key={settings.maxNodes} ref={r => this.maxNodesInput = r} type="number" defaultValue={settings.maxNodes}/>
+                    </Row>
+                </Table>
             </div>
         )
     }
 }
 
-const Table = ({children}) => <table><tbody>{children}</tbody></table>;
+const Table = ({children}) => <table>
+    <tbody>{children}</tbody>
+</table>;
 
-const Row = ({label, children, setFn, id}) => (
-      <tr id={id}>
-          <th style={{paddingRight: 20}}>{label}</th>
-          <td>{children}</td>
-          <td style={{paddingLeft: 20}}><Button bsSize="small" onClick={setFn}>Set</Button></td>
-      </tr>
-);
+
+class Row extends Component {
+    constructor() {
+        super();
+        this.state = {editing: false};
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return (this.state.editing !== nextState.editing) ||
+            (this.state.editing || nextState.editing === false);
+
+    }
+
+    setEditing() {
+        this.setState({editing: true});
+    }
+
+    setValue() {
+        this.setState({editing: false});
+        invoke(this.props, 'setFn');
+    }
+
+    render() {
+        const {label, children, setFn, id, value} = this.props;
+        const {editing} = this.state;
+
+        return (
+            <tr id={id}>
+                <th style={{paddingRight: 20}}>{label}</th>
+                <td style={{width: 300}}>
+                    {editing ? children : value}
+                </td>
+                <td style={{paddingLeft: 20}}>
+                    {editing ? (
+                        <Button bsSize="small" onClick={this.setValue.bind(this)}>Set</Button>
+                    ) : (
+                        <Button bsSize="small" onClick={this.setEditing.bind(this)}>Edit</Button>
+                    )}
+                </td>
+            </tr>
+        )
+    }
+}
+
