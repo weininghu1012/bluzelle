d57fcfb74c63cf7850d1465baa58483a90c8c999
scott <scott@bulldoginfo.com>
2017-10-20 12:54:15 -0700

    added clickTab

--- qa/qa-testing/test/web-functional/features/node-graph-counting-nodes.js
+++ qa/qa-testing/test/web-functional/features/node-graph-counting-nodes.js
@@ -1,13 +1,11 @@
-const _ = require('lodash');
+const {clickTab} = require('../utils');
 
 describe('Node graph tab', () => {
     require('../getBaseElement')('body');
 
-    beforeEach(() => {
-        browser.waitForExist('=Node Graph', 2000);
-        browser.click('=Node Graph');
-    });
-    describe('counting the nodes', () => {
+    beforeEach(() => clickTab('Node Graph'));
+
+    describe('@watch counting the nodes', () => {
         it('should show nodes equals to the number written on screen', () => {
             browser.waitForExist('circle');
             const numberOfNodes = browser.elements('circle').value.length;
