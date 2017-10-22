bf1e9770740e1ba5c66f636eab30579a51c97ba7
scott <scott@bulldoginfo.com>
2017-10-22 16:25:23 -0700

    added icon for linux

--- client/desktop/main.js
+++ client/desktop/main.js
@@ -9,7 +9,7 @@ let win
 
 function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({width: 800, height: 600, icon: __dirname + '/bluzelle.png'});
 
     // and load the index.html of the app.
     win.loadURL(url.format({
