f61d5ccf39f72dbd8a3eefd552a4222132e3816f
scott <scott@bulldoginfo.com>
2017-11-02 08:49:29 -0700

    added ci build for daemon-for-functional-testing

---
+++ dev-ops/ci/build-scripts/daemon-for-functional-testing.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+PID=`ps ax |grep 'Daemon_For_Functional_Testing/client/web/node_modules' | cut -d' ' -f1`
+
+if [ $PID ]; then
+    kill -9 $PID || true
+fi
+
+cd $WORKSPACE/client/web
+yarn
+cd $WORKSPACE/client/web/src
+webpack
+cd $WORKSPACE/client/web
+
+/usr/local/sbin/daemonize -c $WORKSPACE/client/web -e $WORKSPACE/daemon-error -o $WORKSPACE/daemon-stdout -p $WORKSPACE/pid -E BUILD_ID=dontKillMe `which yarn` start
+
