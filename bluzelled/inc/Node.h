813364d9b46ac95d0b90fbfeb2dd0028efb3d81d
dmitrymukhin <dmitry@bluzelle.com>
2017-11-03 09:44:34 -0700

    [kep-46] removeNodes messages sent from every node at the moment it dies.

--- bluzelled/inc/Node.h
+++ bluzelled/inc/Node.h
@@ -1,81 +1,104 @@
 #ifndef KEPLER_NODE_H
 #define KEPLER_NODE_H
 
-#include "Task.h"
+#include <vector>
+
 #include <boost/thread.hpp>
 #include <boost/shared_ptr.hpp>
 
-class Node
-{
+#include "Task.h"
+
+class Node;
+
+typedef std::vector<Node *> Nodes;
+
+#include "web_sockets/Listener.h"
+
+
+class Node {
     // {"address":"0x00","nodeState":"alive","messages":20}
-    std::string                         _name;
-    boost::shared_ptr<Task>             _task;
-    boost::shared_ptr<boost::thread>    _thread;
+    std::string name_;
+    boost::shared_ptr<Task> task_;
+    boost::shared_ptr<boost::thread> thread_;
+    std::weak_ptr<Listener> listener_;
+
 public:
-    Node(uint32_t lifespan = 20, double death_probablity = 0.05)
+    Node(uint32_t lifespan = 20, double death_probablity = 0.05, std::shared_ptr<Listener> l)
+        : listener_(l)
     {
-        auto thread_function = []
+        auto thread_function = [this]
                 (
                         Task *task
                 )
             {
             task->run();
+
+            auto lp = listener_.lock();
+            if (lp !=  nullptr) {
+                std::cout << "sessions: " << lp->sessions_.size() << std::endl;
+                // When run() exits node is dead.
+                for (auto s : lp->sessions_)
+                    {
+                    auto sp = s.lock();
+                    if (!sp)  // corresponding shared_ptr is destroyed (session closed).
+                        {
+                        lp->sessions_.erase(
+                                std::find_if(
+                                        lp->sessions_.begin(),
+                                        lp->sessions_.end(),
+                                        [&sp](auto n)
+                                            { return sp == n.lock(); }));
+                        }
+                    else
+                        sp->send_remove_nodes(name_);
+                    }
+                }
             };
 
-        _task.reset(new Task(lifespan,death_probablity));
-        _thread.reset(new boost::thread
-                (
-                        thread_function,
-                        _task.get()
-                ));
+        task_.reset(new Task(lifespan,death_probablity));
+        thread_.reset(new boost::thread
+                              (
+                                      thread_function,
+                                      task_.get()
+                              ));
 
-        _name = boost::lexical_cast<std::string>(get_thread_id());
+        name_ = boost::lexical_cast<std::string>(get_thread_id());
     }
 
-    boost::thread::id get_thread_id()
-    {
-        return _thread->get_id();
+    boost::thread::id get_thread_id() {
+        return thread_->get_id();
     }
 
-    void kill()
-    {
-        _task->kill();
+    void kill() {
+        task_->kill();
     }
 
-    void join()
-    {
-        _thread->join();
+    void join() {
+        thread_->join();
     }
 
-    Task::State state()
-    {
-        return _task->state();
+    Task::State state() {
+        return task_->state();
     }
 
-    std::string name()
-    {
-        return _name;
+    std::string name() {
+        return name_;
     }
 
-    bool is_joinable()
-    {
-        return _thread->joinable();
+    bool is_joinable() {
+        return thread_->joinable();
     }
 
-    void ping(boost::thread::id other)
-    {
-        _task->ping(other);
+    void ping(boost::thread::id other) {
+        task_->ping(other);
     }
 
-    system_clock::time_point last_change()
-    {
-        return _task->get_last_change();
+    system_clock::time_point last_change() {
+        return task_->get_last_change();
     }
 
 private:
 
 };
-
-typedef std::vector<Node *> Nodes;
 
 #endif //KEPLER_NODE_H
