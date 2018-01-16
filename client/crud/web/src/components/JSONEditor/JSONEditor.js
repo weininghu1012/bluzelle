3a6571eefd34a7cff994abfd6bedce5b716deaf8
Monty Thibault <montythibault@gmail.com>
2018-01-16 11:57:14 -0800

    Revised JSON editor basic functionality.

--- client/crud/web/src/components/JSONEditor/JSONEditor.js
+++ client/crud/web/src/components/JSONEditor/JSONEditor.js
@@ -1,81 +1,153 @@
 import './JSONEditor.css';
 
 
-export const JSONEditor = ({json}) => {
+export class JSONEditor extends Component {
+    render() {
 
-    // If object
-    if (typeof json === 'object' && !Array.isArray(json)) {
-        return <RenderObject json={json}/>;
-    }
+        const {json} = this.props;
 
-    // If array
-    if (Array.isArray(json)) {
-        return renderArray(json);
-    }
+        // If object
+        if (typeof json === 'object' && !Array.isArray(json)) {
+            return <RenderObject json={json}/>;
+        }
 
-    // Standard datatypes
-    return <div>{json.toString()}</div>;
+        // If array
+        if (Array.isArray(json)) {
+            return <RenderArray json={json}/>;
+        }
 
-};
-
-
-
-const triangleRight = '\u25b6';
-const triangleDown = '\u25bc';
-
-class Collapsible extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = { collapsed: props.collapsed || false };
-    }
-
-    toggleCollapse() {
-        this.setState({ collapsed: !this.state.collapsed });
-    }
-
-    collapseTriangle() {
-        return this.state.collapsed ? triangleRight : triangleDown;
-    }
-
+        // Standard datatypes
+        return <RenderField json={json}/>
+    };
 }
 
 
-class RenderObject extends Collapsible  {
+const TRIANGLE_RIGHT = '\u25b6';
+const TRIANGLE_DOWN = '\u25bc';
 
-    render()
-    {
-        const { json } = this.props;
+class Collapsible extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {collapsed: props.collapsed || false};
+    }
+
+    toggleCollapse() {
+        this.setState({collapsed: !this.state.collapsed});
+    }
+
+    collapseTriangle() {
+        return this.state.collapsed ? TRIANGLE_RIGHT : TRIANGLE_DOWN;
+    }
+
+    render() {
+
+        const {children, label} = this.props;
 
         return (
-            <div className='jsoneditor-obj'>
-            <span onClick={() => this.toggleCollapse()}>
-                {this.collapseTriangle()} {'{}'} ({Object.entries(json).length} entries)
-            </span>
-                <div className='jsoneditor-indent'>
-                    {this.state.collapsed || keyValuePairs(json)}
+            <div style={{ paddingLeft: 20 }}>
+                <span onClick={() => this.toggleCollapse()}>
+                    {this.collapseTriangle()} {label}
+                </span>
+                <div>
+                    {this.state.collapsed || children}
                 </div>
             </div>
         );
     }
 }
 
-const keyValuePairs = json => Object.entries(json).map(([key, value]) =>
-    <div key={key}>
-        <span>{key}</span>: <JSONEditor key={key} name={key} json={value}/>
-    </div>);
 
-
-const renderArray = json => (
-    <div className='jsoneditor-arr'>
-        {triangleRight} {'[]'} ({json.length} entries)
-        <div className='jsoneditor-indent'>
-            {indexValuePairs(json)}
-        </div>
-    </div>
+const RenderObject = ({json}) => (
+    <Collapsible label={`{} (${Object.entries(json).length} entries)`}>
+        {
+            Object.entries(json).map(([key, value]) =>
+                <div key={key}>
+                    <span>{key}</span>: <JSONEditor json={value}/>
+                </div>)
+        }
+    </Collapsible>
 );
 
-const indexValuePairs = json => json.map((value, index) =>
-    <div key={index}>
-        <span>({index})</span>: <JSONEditor name={''} json={value}/>
-    </div>);
+const RenderArray = ({json}) => (
+    <Collapsible label={`[] (${json.length} entries)`}>
+        {
+            json.map((value, index) =>
+                <div key={index}>
+                    <span>{index}</span>: <JSONEditor json={value}/>
+                </div>)
+        }
+    </Collapsible>
+);
+
+
+class RenderField extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            formValue: props.json.toString(),
+            formActive: false
+        };
+    }
+
+    handleChange(event) {
+        this.setState({
+            formValue: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        this.setState({
+            formActive: false
+        });
+
+        // Propagate change
+    }
+
+    enableEditing() {
+        this.setState({
+            formActive: true
+        });
+    }
+
+    render() {
+        const {json} = this.props;
+
+        let color = colorFromType(json);
+
+        return (
+            <span style={{color}} onClick={this.enableEditing.bind(this)}>
+              {this.state.formActive ?
+                  <form onSubmit={this.handleSubmit.bind(this)}>
+                      <input type='text' value={this.state.formValue} onChange={this.handleChange.bind(this)}/>
+                  </form>
+                  : json.toString()}
+            </span>
+        );
+    }
+};
+
+const colorFromType = json => {
+    let color;
+    switch(typeof json) {
+        case 'string':
+            color = 'blue';
+            break;
+
+        case 'number':
+            color = 'red';
+            break;
+
+        case 'boolean':
+            color = 'purple';
+            break;
+
+        default:
+            color = 'pink';
+    }
+
+    return color;
+};
