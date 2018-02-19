35c8365b3cf9c62e10af51506efa615b62156c18
jyclam <johnlamyc@gamil.com>
2018-02-19 15:59:12 -0800

    Tests for node-list: populated and status change

--- qa/qa-testing/test/web-functional/features/header.specs.js
+++ qa/qa-testing/test/web-functional/features/header.specs.js
@@ -2,6 +2,12 @@ describe('web page header', () => {
     const header = require('../getBaseElement')('header');
 
     it('should exist', () => {
-        expect(header().getAttribute('div>img', 'src')).to.contain('data');
+        expect(browser.getAttribute('div>img', 'src')).to.contain('data');
+    });
+
+    it('should exist', () => {
+        browser.waitForExist('a=Node List');
+        browser.click('a=Node List');
+        browser.waitForExist('span=127.0.0.1:8200');
     });
 });
--- qa/qa-testing/test/web-functional/features/node-list.specs.js
+++ qa/qa-testing/test/web-functional/features/node-list.specs.js
@@ -1,16 +1,41 @@
+const _ = require('lodash');
 describe('Node List tab', () => {
+    const header = require('../getBaseElement')('div.react-grid-Main');
+    const NUM_OF_NODES = 5;
+
+    beforeEach(() => {
+        browser.waitForExist('=Node List');
+        browser.click('=Node List');
+        emulator.setMaxNodes(NUM_OF_NODES);
+    });
 
     describe('Table Headers', () => {
-        const header = require('../getBaseElement')('div.react-grid-HeaderRow');
 
-        beforeEach(() => {
-            browser.waitForExist('=Node List', 2000);
-            browser.click('=Node List');
+        it('should contain table headers', () => {
+            ['Address', 'Status', 'Actions'].forEach(text => {
+                header().waitForExist(`div.widget-HeaderCell__value*=${text}`);
+            });
         });
-       it('should contain table headers', () => {
-          ['Address', 'Status', 'Actions'].forEach(text => {
-            header().waitForExist(`div.widget-HeaderCell__value*=${text}`);
-          });
-       });
+    });
+
+    describe('Table Rows', () => {
+        it('should show all nodes', () => {
+            browser.waitUntil(() => {
+                return browser.elements('div.react-grid-Canvas>div>div').value.length === 5
+            }, 5000, 'expected nodes to equal number set by emulator');
+        });
+
+        it('should show all nodes to be alive', () => {
+            browser.waitUntil( () =>
+                browser.elements('div.react-grid-Canvas>div>div').value.length === 5
+            );
+
+            ['new', 'alive'].forEach( status =>
+                _.times(5).forEach( idx =>
+                    browser.elements('div.react-grid-Canvas>div>div').value[idx]
+                        .waitForExist(`div=${status}`)
+                )
+            );
+        })
     });
 });
