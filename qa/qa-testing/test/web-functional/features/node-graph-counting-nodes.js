fd8e42fc77efcdd2078c6c8ef523dd3cf1749e3d
jyclam <johnlamyc@gamil.com>
2018-02-21 12:22:46 -0800

    Add test for node-graph to count nodes

--- qa/qa-testing/test/web-functional/features/node-graph-counting-nodes.js
+++ qa/qa-testing/test/web-functional/features/node-graph-counting-nodes.js
@@ -6,7 +6,9 @@ describe('Node graph tab', () => {
     beforeEach(() => clickTab('Node Graph'));
 
     describe('counting the nodes', () => {
-        it('should show nodes equals to the number written on screen', () => {
+        it('should show nodes equal to the number written on screen', () => {
+            browser.waitForExist('div=1 Nodes');
+            browser.waitUntil(() => browser.elements('circle').value.length === 2);
         });
     });
 });
