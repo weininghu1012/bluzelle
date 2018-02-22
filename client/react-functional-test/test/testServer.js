e7cf590867b58bebcabf1ccf30299685e242db24
Monty Thibault <montythibault@gmail.com>
2018-02-22 22:15:41 +0100

    React test app working with no webpack.

--- client/react-functional-test/test/testServer.js
+++ client/react-functional-test/test/testServer.js
@@ -2,9 +2,11 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+
 let server;
 
 export const start = function(port=8200) {
+    addReact();
 
     const filename = path.resolve(__dirname + '/test-app/index.html');
 
@@ -19,5 +21,30 @@ export const start = function(port=8200) {
 
 
 export const close = function() {
+    removeReact();
     server && server.close();
+};
+
+
+
+const reactLink = __dirname + '/test-app/react.js';
+
+const addReact = function() {
+    if(fs.existsSync(reactLink)) {
+        return;
+    }
+
+    const reactDir = path.dirname(require.resolve('react'));
+    const reactPath = reactDir + '/umd/react.production.min.js';
+
+    fs.symlinkSync(reactPath, reactLink);
+
+};
+
+const removeReact = function() {
+    if(!fs.existsSync(reactLink)) {
+        return;
+    }
+
+    fs.unlinkSync(reactLink);
 };
