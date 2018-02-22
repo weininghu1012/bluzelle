a90072d5ed5cabfffcabe145222136726726b6c5
jyclam <johnlamyc@gamil.com>
2018-02-22 10:10:58 -0800

    Change 'node added' test for log.specs

--- qa/qa-testing/test/web-functional/features/log.specs.js
+++ qa/qa-testing/test/web-functional/features/log.specs.js
@@ -5,18 +5,19 @@ describe('Log tab', () => {
     describe('Table Headers', () => {
         const header = require('../getBaseElement')('div.react-grid-HeaderRow');
 
-        it('should contain table headers', () =>{
-            ['Level', 'Timestamp', 'Message', 'Node'].forEach(text =>{
+        it('should contain table headers', () => {
+            ['Level', 'Timestamp', 'Message', 'Node'].forEach( text =>{
                 header().waitForExist (`div.widget-HeaderCell__value*=${text}`);
             });
         });
     });
 
     describe('Table rows', () => {
-        it('@watch should log node added', () => {
-            emulator.setMaxNodes(2)
-            browser.elements('div.react-grid-Canvas>div>div').value[0]
-                .waitForExist('div=Node Added');
+        it('should log node added', () => {
+            ['info', new Date().toISOString().substring(0,11), 'Node added', '127.0.0.1'].forEach( text => {
+                browser.elements('div.react-grid-Canvas>div>div').value[0]
+                    .waitForExist(`div*=${text}`);
+            });
         });
 
         it('should display a log entry when one is received', () =>  {
