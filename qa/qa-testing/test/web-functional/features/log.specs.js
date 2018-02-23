a3a13d8d889232265c142077c1f72c6f94affaad
jyclam <johnlamyc@gamil.com>
2018-02-23 12:38:20 -0800

    Test 'node added' log for one node

--- qa/qa-testing/test/web-functional/features/log.specs.js
+++ qa/qa-testing/test/web-functional/features/log.specs.js
@@ -1,37 +1,21 @@
-import _ from 'lodash'
-
 describe('Log tab', () => {
-    beforeEach(() => {
-        emulator.setMaxNodes(4);
-        console.log('node count:', emulator.getNodesCount());
-    });
-
     describe('Table Headers', () => {
         const header = require('../getBaseElement')('div.react-grid-HeaderRow');
-
         it('should contain table headers', () => {
-            ['Level', 'Timestamp', 'Message', 'Node'].forEach( text =>{
+            ['Level', 'Timestamp', 'Message', 'Node'].forEach( text => {
                 header().waitForExist (`div.widget-HeaderCell__value*=${text}`);
             });
         });
     });
 
     describe('Table rows', () => {
-
-        _.times(emulator.getNodesCount(), idx => {
-            console.log('index:', idx);
-            it('@watch should log node added', () => {
-
-                ['info', new Date().toISOString().substring(0, 11), 'Node added', '127.0.0.1'].forEach(text => {
-                    browser.elements('div.react-grid-Canvas>div>div').value[idx]
-                        .waitForExist(`div*=${text}`);
-                });
+        const header = require('../getBaseElement')('div.react-grid-Grid');
+        it('should log node added', () => {
+            ['info', new Date().toISOString().substring(0, 11), 'Node added', '127.0.0.1'].forEach( text => {
+                header().waitForExist('div.react-grid-Canvas>div>div');
+                browser.elements('div.react-grid-Canvas>div>div').value[0]
+                    .waitForExist(`div*=${text}`);
             });
-        });
-
-        it(' should display a log entry when one is received', () =>  {
-            console.log('Node count:', emulator.getNodesCount());
-            console.log('Node value:', emulator.getNodes()[0].alive);
         });
     });
 });
