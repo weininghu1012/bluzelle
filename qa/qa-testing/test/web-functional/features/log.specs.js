d6b840e6fcba3b01a7aed7252f66f10bd4a2b521
jyclam <johnlamyc@gamil.com>
2018-02-22 17:27:24 -0800

    Timing issue looping tests based on emulator.getNodesCount

--- qa/qa-testing/test/web-functional/features/log.specs.js
+++ qa/qa-testing/test/web-functional/features/log.specs.js
@@ -1,7 +1,11 @@
-import {sendLogMessage} from "../CommunicationService";
 import _ from 'lodash'
 
 describe('Log tab', () => {
+    beforeEach(() => {
+        emulator.setMaxNodes(4);
+        console.log('node count:', emulator.getNodesCount());
+    });
+
     describe('Table Headers', () => {
         const header = require('../getBaseElement')('div.react-grid-HeaderRow');
 
@@ -13,15 +17,21 @@ describe('Log tab', () => {
     });
 
     describe('Table rows', () => {
-        it('should log node added', () => {
-            ['info', new Date().toISOString().substring(0,11), 'Node added', '127.0.0.1'].forEach( text => {
-                browser.elements('div.react-grid-Canvas>div>div').value[0]
-                    .waitForExist(`div*=${text}`);
+
+        _.times(emulator.getNodesCount(), idx => {
+            console.log('index:', idx);
+            it('@watch should log node added', () => {
+
+                ['info', new Date().toISOString().substring(0, 11), 'Node added', '127.0.0.1'].forEach(text => {
+                    browser.elements('div.react-grid-Canvas>div>div').value[idx]
+                        .waitForExist(`div*=${text}`);
+                });
             });
         });
 
-        it('should display a log entry when one is received', () =>  {
-            _.times(5, (num) => sendLogMessage(`some message ${num}`));
+        it(' should display a log entry when one is received', () =>  {
+            console.log('Node count:', emulator.getNodesCount());
+            console.log('Node value:', emulator.getNodes()[0].alive);
         });
     });
 });
