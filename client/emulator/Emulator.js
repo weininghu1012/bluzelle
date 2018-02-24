04dc9769835d28fcd38aad43460566bc5c958b24
jyclam <johnlamyc@gamil.com>
2018-02-23 18:09:35 -0800

    Remove extra comma

--- client/emulator/Emulator.js
+++ client/emulator/Emulator.js
@@ -23,7 +23,7 @@ module.exports = {
     getData: getData,
     setData: setData,
     behaveRandomly: (v) => behaveRandomly.set(v),
-    isRandom: () => behaveRandomly.get(),
+    isRandom: () => behaveRandomly.get()
 };
 
 
