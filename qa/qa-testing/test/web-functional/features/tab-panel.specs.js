a0ffd6f13ba940ac46bbb12e15a6f15561276e28
jyclam <johnlamyc@gamil.com>
2018-02-26 12:34:39 -0800

    Add Message List tab test

--- qa/qa-testing/test/web-functional/features/tab-panel.specs.js
+++ qa/qa-testing/test/web-functional/features/tab-panel.specs.js
@@ -1,8 +1,9 @@
 describe('The tab panel', () => {
     const tabs = require('../getBaseElement')('ul.nav-tabs');
 
-    it('should exist', () => {
+    it('@watch should exist', () => {
         tabs().waitForExist('a=Log');
+        tabs().waitForExist('a=Message List');
         tabs().waitForExist('a=Node List');
         tabs().waitForExist('a=Node Graph');
     });
